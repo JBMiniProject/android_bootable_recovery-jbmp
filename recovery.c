3b19e8db56e71a32e2447a909e26a4bc084f3d22
Daveee10 <daver16@msn.com>
2012-09-29 20:57:45 +0200

    Disabling back menu button

--- recovery.c
+++ recovery.c
@@ -497,12 +497,12 @@ get_menu_selection(char** headers, char** items, int menu_only,
             if (wrap_count == 3) {
                 wrap_count = 0;
                 if (ui_get_showing_back_button()) {
-                    ui_print("Back menu button disabled.\n");
-                    ui_set_showing_back_button(0);
+                    //ui_print("Back menu button disabled.\n");
+                    //ui_set_showing_back_button(0);
                 }
                 else {
-                    ui_print("Back menu button enabled.\n");
-                    ui_set_showing_back_button(1);
+                    //ui_print("Back menu button enabled.\n");
+                    //ui_set_showing_back_button(1);
                 }
             }
         }
